78327886b9f7222424da0b1c423fe9b09fefd6f4
mitali2608 <mitalibawankule21@gmail.com>
2021-09-15 16:07:30 +0530

    Players Added

---
+++ Players.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace snake_ladder
+{
+    class Players 
+ {
+       public string name;
+       public int CurrentPlace;
+       public int PreviousPlace;
+       
+  
+  public Players()		 
+{
+            name = "";
+            CurrentPlace = 1;
+            PreviousPlace = 1;
+
+}
+        
+
+public int DiceRoll()        
+{
+            Random random = new Random();
+            return random.Next(1,7);  
+}
+
+
+}
+}
